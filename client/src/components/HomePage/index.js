23bb4ba4e1a625bbd8b20fcf2427d085ecf59d9f
Chris Mendoza <cmend85@gmail.com>
2018-07-16 23:47:48 -0500

    Added styles

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 
+import { Fade } from 'react-slideshow-image';
+import image1 from '../../images/Cedar_park_home.png'
+import image2 from '../../images/Cedar_park_home_interior_1.png'
+// import image3 from '../../images/Cedar_park_home_3.jpg'
+// import image4 from '../../images/Cedar_park_home_2_interior.jpg'
+// import image5 from '../../images/Cedar_park_home_2.png'
+import styles from './HomePage.css'
+
+const images = [
+  image1,
+  '../../components/images/Cedar_park_home_interior_1.png',
+  // '../../images/Cedar_park_home_3.jpg',
+  // '../../images/Cedar_park_home_2_interior.jpg',
+  // '../../images/Cedar_park_home_2.png'
+]
+
+
+
 class HomePage extends Component {
 
+  
+
   render() {
+    const divStyle = {
+      backgroundImage: 'url(' + image1 + ')',
+      height: '800px',
+      width: '100%',
+      backgroundPosition: 'center center',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed',
+      backgroundSize: 'cover'
+    }
     return (
-      <div>
-        Ayy this is the home page
+      <div style={ divStyle }>
       </div>
     )
   }
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,9 +6,33 @@ class Navbar extends Component {
 
   render () {
     return (
-      <div className={ styles.example }>
-        Hi this is a nav bar
-      </div>
+        <ul className={styles.container}>
+          <li className={styles.mainHeader}>
+            Sharp Appraisers
+          </li>
+
+          <li className={ styles.header }>
+            Home
+          </li>
+          <li className={ styles.header }>
+            Homeowner
+          </li>
+          <li className={ styles.header }>
+            Quote
+          </li>
+          <li className={ styles.header }>
+            Order
+          </li>
+          <li className={ styles.header }>
+            Coverage
+          </li>
+          <li className={ styles.header }>
+            Contact
+          </li>
+          <li className={ styles.header }>
+            About 
+          </li>
+        </ul>
     )
   }
 }
