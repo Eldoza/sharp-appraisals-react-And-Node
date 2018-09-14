2fd92c68fd6630087670f637e3fae242acf3a6e9
Chris Mendoza <cmend85@gmail.com>
2018-09-14 01:21:48 -0500

    Moved mailgun key to env file

--- email-configs/email-config.js
+++ email-configs/email-config.js
@@ -1,6 +1,6 @@
 module.exports = () => {
     const emailCongfig = {
-        apiKey: '5f0c69eb38b98ef0ed7fb56d72951749-a4502f89-44cf261e',
+        apiKey: `${process.env.REACT_APP_MAILGUN_API_KEY}`,
         domain: 'https://api.mailgun.net/v3/sandbox13f57b4067a3489994e01641bbfe43d0.mailgun.org'
     }
 
